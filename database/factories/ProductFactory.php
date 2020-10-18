b30370c4ddb77709f0eceadba136f544fa00cbf0
abderrahmen-bmz <abdogray@gmail.com>
2020-10-18 12:20:31 +0100

    fix issue

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -12,6 +12,6 @@ $factory->define(Product::class, function (Faker $faker) {
         'unit'=> $faker->randomElement(['Kilio gram','quantity','meter']),
         'price'=> $faker->randomFloat(2,10,500),
         'total'=> $faker->numberBetween(2 , 250),
-        'category'  => $faker->numberBetween(1,50),
+        'category_id'  => $faker->numberBetween(1,50),
     ];
 });
